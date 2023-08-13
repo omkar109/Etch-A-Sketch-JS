48ea29997b0f44ed9e7911b7f24792ff377e95b8
Omkar Kendale <omkarkendale9@gmail.com>
2023-08-13 18:04:08 -0500

    User can change grid size

--- draw.js
+++ draw.js
@@ -1,15 +1,37 @@
 const container = document.querySelector(".container");
-let numSquares = 64;
+let numSquares = 16; //Represents number of squares in width/length
 const button = document.querySelector("button");
 container.style.height = `${window.innerHeight - button.style.height}px`;
-for(let i = 0; i < numSquares; i++){
-    newDiv = document.createElement("div");
-    newDiv.classList.add(`childDiv`);
-    newDiv.style.width = `${100/Math.sqrt(numSquares)}%`;
-    newDiv.style.height = `${100/Math.sqrt(numSquares)}%`;
-    newDiv.textContent = `div${window.innerWidth}, ${newDiv.style.width}`;
-    newDiv.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "blue";
-    })
-    container.appendChild(newDiv);
+generateGrid = function(numSquares){
+    for(let i = 0; i < numSquares*numSquares; i++){
+        newDiv = document.createElement("div");
+        newDiv.classList.add(`childDiv`);
+        newDiv.style.width = `${100/numSquares}%`;
+        newDiv.style.height = `${100/numSquares}%`;
+        //newDiv.textContent = `div${window.innerWidth}, ${newDiv.style.width}`;
+        newDiv.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "blue";
+        })
+        container.appendChild(newDiv);
+    }
 }
+
+clearGrid = function(){
+    childDivs = document.querySelectorAll(".childDiv");
+    childDivs.forEach((childDiv) => {
+        childDiv.remove();
+    })
+}
+
+changeGrid = function(numSquares){
+    clearGrid();
+    generateGrid(numSquares);
+}
+
+button.addEventListener("click", (event) => {
+    numSquares = prompt("How many pixels wide do you want the grid? 100 pixel limit");
+    if(parseInt(numSquares) > 100) numSquares = 100;
+    changeGrid(parseInt(numSquares));
+})
+
+generateGrid(numSquares);
