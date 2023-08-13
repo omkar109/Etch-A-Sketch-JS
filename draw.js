4bfa9406df6fd15a8f17f513d9b6b5d7871f3dad
Omkar Kendale <omkarkendale9@gmail.com>
2023-08-13 17:43:27 -0500

    Added button and changed div height and width to percentages for consistent spacing

--- draw.js
+++ draw.js
@@ -1,15 +1,15 @@
 const container = document.querySelector(".container");
-let numSquares = 9;
+let numSquares = 64;
+const button = document.querySelector("button");
+container.style.height = `${window.innerHeight - button.style.height}px`;
 for(let i = 0; i < numSquares; i++){
     newDiv = document.createElement("div");
     newDiv.classList.add(`childDiv`);
-    newDiv.style.width = `${window.innerWidth/Math.sqrt(numSquares)}px`;
-    newDiv.style.height = `${window.innerHeight/Math.sqrt(numSquares)}px`;
-    //newDiv.textContent = `div${window.innerWidth}, ${newDiv.style.width}`;
+    newDiv.style.width = `${100/Math.sqrt(numSquares)}%`;
+    newDiv.style.height = `${100/Math.sqrt(numSquares)}%`;
+    newDiv.textContent = `div${window.innerWidth}, ${newDiv.style.width}`;
     newDiv.addEventListener("mouseover", (event) => {
         event.target.style.backgroundColor = "blue";
     })
     container.appendChild(newDiv);
 }
-
-console.log(window.innerWidth);
